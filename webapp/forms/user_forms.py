c1ca71632b6230a07def5a45bc14d0f438981803
Ariadna Cortés Pereles <ariadna.cortes@code-ing.com>
2018-03-21 09:28:13 -0700

    Include login view

--- webapp/forms/user_forms.py
+++ webapp/forms/user_forms.py
@@ -7,17 +7,20 @@ from alumnica_model.models import LearnerModel, AuthUser
 class UserForm(forms.ModelForm):
     password1 = forms.CharField(widget=forms.PasswordInput())
     password2 = forms.CharField(widget=forms.PasswordInput())
-
+    user_type = forms.CharField()
     class Meta:
         model = AuthUser
         fields = ['username', 'email', 'password1', 'password2', 'user_type']
 
     def __init__(self, *args, **kwargs):
         super(UserForm, self).__init__(*args, **kwargs)
-        self.fields['user_type'] = UserType.LEARNER
+        self.fields['user_type'].initial = UserType.LEARNER
 
 
 class LearnerForm(forms.ModelForm):
     class Meta:
         model = LearnerModel
         exclude = ['auth_user_field']
+
+class UserLoginForm(forms.ModelForm):
+    pass
--- webapp/views/user_views.py
+++ webapp/views/user_views.py
@@ -1,12 +1,50 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
-from django.views.generic.base import TemplateView, View
-from webapp.forms.user_forms import LearnerForm, UserForm
+from django.views.generic.base import TemplateView, View, RedirectView
+
+from alumnica_model.models import AuthUser
+from webapp.forms.user_forms import LearnerForm, UserForm, UserLoginForm
 
 
 class IndexView(TemplateView):
     template_name = 'webapp/pages/index.html'
 
+
 class LoginView(View):
+    class LoginView(View):
+        form_class = UserLoginForm
+        template_name = "studio/pages/login.html"
+        success_url = '/users/profile/'
+
+        def get(self, request):
+            if request.user.is_authenticated and not request.user.is_staff:
+                return HttpResponseRedirect('/users/profile')
+            else:
+                form = UserLoginForm(None)
+                return render(request, "studio/pages/login.html", {'form': form})
+
+        def post(self, request):
+            form = UserLoginForm(data=request.POST)
+            if form.is_valid:
+                username = request.POST['username']
+                password = request.POST['password']
+                try:
+                    user = AuthUser.objects.get(username=username)
+                    print(username)
+                    print(password)
+
+                    if user.check_password(password) and not user.is_staff:
+                        login(request, user)
+                        return redirect('/users/profile')
+                except AuthUser.DoesNotExist:
+                    form = UserLoginForm(None)
+                    return render(request, "webapp/pages/login.html", {'form': form})
+
+            print('form not valid')
+            form = UserLoginForm(None)
+            return render(request, "webapp/pages/login.html", {'form': form})
+
+class LogoutView(RedirectView):
     pass
 
 class SignUpView(View):
