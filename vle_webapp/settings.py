c5a1b7b4a4e9e1f65e1aecc44bd2755307dc5aaf
José Luis Valencia Herrera <jose.valencia@code-ing.com>
2018-04-05 03:28:51 -0500

    Add whitenoise

--- vle_webapp/settings.py
+++ vle_webapp/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -79,7 +80,6 @@ WSGI_APPLICATION = 'vle_webapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 
 DATABASES = {
     'default': {
