04051ae3ba71a339b45a980dcb67316fc7c481c7
Kinich Barcelo <kinich.barcelo@code-ing.com>
2018-07-10 18:35:00 -0500

    Dashboard ambito grid slide, recent activity and Insignias
    Made the slider for all the ambitos
    corrected all styles for recent acitvity
    added insignias

---
+++ webapp/static/webapp/js/dashboard.js
@@ -0,0 +1,19 @@
+$('#materias-slider').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    items: 4,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3500,
+    autoplayHoverPause: true,
+  });
+
+
+  $('#insignias-slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    items: 4,
+    dots: false,
+  });
