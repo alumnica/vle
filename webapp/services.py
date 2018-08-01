a115584d9ab16aa68633c40653e3fbae4f315d00
Ariadna Cortés Perales <ariadna.cortes@code-ing.com>
2018-08-01 12:12:02 -0500

    Changes xapi credentials to environment variables

--- webapp/services.py
+++ webapp/services.py
@@ -1,18 +1,16 @@
 import json
-import os
-
 import requests
-
+from vle_webapp.settings import XAPI_URL, XAPI_VERSION, XAPI_KEY
 from webapp.statements import ComplexHandler
 
 
 def send(statement):
-    url = 'http://ec2-54-183-159-44.us-west-1.compute.amazonaws.com/data/xAPI/statements'
+    url = XAPI_URL
     data = json.dumps(statement, default=ComplexHandler)
     headers = {
-        'X-Experience-API-Version': '1.0.1',
+        'X-Experience-API-Version': XAPI_VERSION,
         'Content-Type': 'application/json',
-        'Authorization': 'Basic YTU5YjNlOTE3NWI5ODVkYTc4Nzg4MGFjODUzZTFhZjU3Nzk3ZGM1ZDpiN2U2OTEzMjc0NzQyYjFjYzY2YjVhODVkMmZlOTVmODg5NTc0NGRm'
+        'Authorization': XAPI_KEY
     }
     print(data)
     response = requests.post(url=url, data=data, headers=headers)
