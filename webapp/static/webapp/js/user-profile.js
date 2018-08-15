bc48971bf3e65983bd76c7c2957e5c796ab22777
Kinich Barcelo <kinich.barcelo@code-ing.com>
2018-08-14 19:16:28 -0500

    add tags to search, user porfile questions, blog to dashboard

--- webapp/static/webapp/js/user-profile.js
+++ webapp/static/webapp/js/user-profile.js
@@ -15,3 +15,39 @@ $('.avatar-options').on('click', 'img', function () {
         }
     });
 });
+
+$('#user-info-btn').on('click', function(){
+    let matFav = $('#materia-fav').val(),
+        hora = $('#horario').val(),
+        uni = $('#univ').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/profile_info',
+        data: {
+            'favourite_subject': matFav,
+            'working_time': hora,
+            'university_studies': uni,
+            'learner': pk
+        },
+        success: function(){
+            alert('saved');
+        }
+    })
+});
+
+
+$(document).ready(function () {
+    $.ajax({
+        type: 'GET',
+        url: '/api/profile_info',
+        data: function(data){
+            $('#materia-fav').val(data.favourite_subject);
+            $('#horario').val(data.working_time);
+            $('#univ').val(data.university_studies);
+        },
+        success: function(){
+            alert('recieved');
+        }
+    })
+});
