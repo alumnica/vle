fa8916cc1a0bf3ae58ceb47f93fb8853411204e6
Ariadna Cortés Perales <ariadna.cortes@code-ing.com>
2018-08-23 11:08:23 -0500

    Deletes translations in urls

--- webapp/urls/ambit_urls.py
+++ webapp/urls/ambit_urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from django.utils.translation import gettext_lazy as _
 
 from webapp.views.ambit_views import *
 
 urlpatterns = [
-    path(_('ambit_grid/'), AmbitGridView.as_view(), name='ambitos-grid_view'),
+    path('ambit_grid/', AmbitGridView.as_view(), name='ambitos-grid_view'),
 ]
--- webapp/urls/profile_urls.py
+++ webapp/urls/profile_urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from django.utils.translation import gettext_lazy as _
 
 from webapp.views.profile_views import *
 
 urlpatterns = [
-    path(_('first-login-info/'), FirstLoginInfoView.as_view(), name='first-login-info_view'),
-    path(_('first-login-p1/'), FirstLoginP1View.as_view(), name='first-login-p1_view'),
-    path(_('learning_quiz/'), LargeLearningStyleQuizView.as_view(), name='large_learning_quiz_view'),
+    path('first-login-info/', FirstLoginInfoView.as_view(), name='first-login-info_view'),
+    path('first-login-p1/', FirstLoginP1View.as_view(), name='first-login-p1_view'),
+    path('learning_quiz/', LargeLearningStyleQuizView.as_view(), name='large_learning_quiz_view'),
     path('', ProfileSettingsView.as_view(), name='profile_view')
 ]
