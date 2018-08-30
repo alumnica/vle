264a7bd7f0e3c2ca95f7c76e879c9aa50d838ebb
Ariadna Cortés Perales <ariadna.cortes@code-ing.com>
2018-08-30 12:46:10 -0500

    Fix #29

--- webapp/static/webapp/js/search.js
+++ webapp/static/webapp/js/search.js
@@ -3,6 +3,9 @@ input.addEventListener("keyup", function (event) {
     event.preventDefault();
     if (event.keyCode === 13) {
         let text = this.value;
+
+        //text = text.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+
         window.location.href = "/search/search_odas/" + text + "/";
     }
 });
--- webapp/urls/navigation_urls.py
+++ webapp/urls/navigation_urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from webapp.views.navigation_views import SearchView, RecentActivitiesView
 
 urlpatterns = [
-    path('search_odas/<slug:text>/', SearchView.as_view(), name='search_view'),
+    path(u'search_odas/<str:text>/', SearchView.as_view(), name='search_view'),
     path('recent_activity/', RecentActivitiesView.as_view(), name='recent_activity_view'),
 ]
