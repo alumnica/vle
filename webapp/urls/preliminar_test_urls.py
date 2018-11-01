bb7e252c56d0f20883c203963175753eae187fb7
Ariadna Cortés Perales <ariadna.cortes@code-ing.com>
2018-10-31 18:10:13 -0600

    Reformat code and optimize imports in urls

--- webapp/urls/preliminar_test_urls.py
+++ webapp/urls/preliminar_test_urls.py
@@ -6,7 +6,8 @@ from webapp.views.preliminar_test_views import LoginTestView, SignUpTestView, Si
 urlpatterns = [
     path('login/', LoginTestView.as_view(), name='login_test_view'),
     path('signup/', SignUpTestView.as_view(), name='signup_test_view'),
-    path('confirm_signup/<str:uidb64>, <str:token>', SignUpTestConfirmation.as_view(), name='signup_test_confirmation_view'),
+    path('confirm_signup/<str:uidb64>, <str:token>', SignUpTestConfirmation.as_view(),
+         name='signup_test_confirmation_view'),
     path('confirmation_error/<int:pk>/', SignupTestConfirmationError.as_view(), name='confirmation_test_error_view'),
     path('first_login_info/', FirstLoginTestInfoView.as_view(), name='first_login_test_info_view'),
     path('first_login_p1/', FirstLoginTestP1View.as_view(), name='first_login_test_p1_view'),
