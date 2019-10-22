7c9d38bb2fd05f8779ee7b87057f0cc3f24b9a1a
amartinez <amartinez@ecosoft.com.mx>
2019-10-22 13:03:37 -0500

    se agrega path a S3 y la region

--- vle_webapp/settings.py
+++ vle_webapp/settings.py
@@ -209,6 +209,9 @@ if all([AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME]):
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
+    AWS_S3_REGION_NAME = "us-west-1"
+    AWS_S3_ADDRESSING_STYLE = "virtual"
+
 
 AUTH_USER_MODEL = 'alumnica_model.AuthUser'
 
